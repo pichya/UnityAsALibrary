828900c258e512c0d4025dc20070cacbd01e2794
pichya <pichya69@hotmail.com>
2020-05-21 01:27:31 -0400

    Firebase added
    Created Firebase database functions and modified text and video assets

--- TenTalesARQuiz/Assets/10TalesDemo/CheckAnswer.cs
+++ TenTalesARQuiz/Assets/10TalesDemo/CheckAnswer.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 public class CheckAnswer : MonoBehaviour
 {
@@ -19,6 +23,9 @@ public class CheckAnswer : MonoBehaviour
     int timer;
     public float timeRemaining = 60;
     bool timerActive = true;
+    
+    int incorrect = 0;
+    int correct = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +37,16 @@ public class CheckAnswer : MonoBehaviour
         text4.SetActive(false);
         ClearDisplay();
         counterTextPro.text = "Timer: 60";
+        //
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://tentalesarquiz.firebaseio.com/");
+
+        // Get the root reference location of the database.
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        
+        long quiztime = 15;
+        //
+        reference.Child("users").Child("timeelapsed").SetValueAsync(quiztime);
     }
     
     void ClearDisplay(){
@@ -40,7 +57,7 @@ public class CheckAnswer : MonoBehaviour
     
     void OnTriggerEnter(Collider coll){
         ClearDisplay();
-        
+        //
         if (coll.gameObject.name == "ApplePrefab"){
             if (timerActive){
                 timer = 0;
@@ -48,6 +65,7 @@ public class CheckAnswer : MonoBehaviour
                 correctDisplay.SetActive(true);
                 if (timerActive){
                     timerActive = false;
+                    correct = 1;
                     StopTheClock();
                 }
             }
@@ -56,18 +74,27 @@ public class CheckAnswer : MonoBehaviour
             if (timerActive){
                 timer = 0;
                 wrongDisplay.SetActive(true);
+                incorrect++;
+                DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+                reference.Child("users").Child("incorrect").SetValueAsync(incorrect);
             }
         }
         if (coll.gameObject.name == "RiverPrefab"){
             if (timerActive){
                 timer = 0;
                 wrongDisplay.SetActive(true);
+                incorrect++;
+                DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+                reference.Child("users").Child("incorrect").SetValueAsync(incorrect);
             }
         }
         if (coll.gameObject.name == "SledPrefab"){
             if (timerActive){
                 timer = 0;
                 wrongDisplay.SetActive(true);
+                incorrect++;
+                DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+                reference.Child("users").Child("incorrect").SetValueAsync(incorrect);
             }
         }
     }
@@ -79,6 +106,16 @@ public class CheckAnswer : MonoBehaviour
         text2.SetActive(true);
         text3.SetActive(true);
         text4.SetActive(true);
+        //
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        
+        long quiztime = 60 - Mathf.FloorToInt(timeRemaining);
+        string resultstring = correct + " of " + (incorrect+correct);
+        //
+        reference.Child("users").Child("correct").SetValueAsync(correct);
+        reference.Child("users").Child("incorrect").SetValueAsync(incorrect);
+        reference.Child("users").Child("result").SetValueAsync(resultstring);
+        reference.Child("users").Child("timeelapsed").SetValueAsync(quiztime);
     }
     
     // Update is called once per frame
--- TenTalesARQuiz/Assets/Scripts/SetTargetFramerate.cs
+++ TenTalesARQuiz/Assets/Scripts/SetTargetFramerate.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 /// <summary>
 /// Simple MonoBehaviour to set the Application's targetFrameRate
@@ -28,9 +32,37 @@ public class SetTargetFramerate : MonoBehaviour
     {
         Application.targetFrameRate = targetFrameRate;
     }
-
+    
+    public DateTime starttime;
     void Start()
     {
         SetFrameRate();
+        // Set up the Editor before calling into the realtime database
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://tentalesarquiz.firebaseio.com/");
+
+        // Get the root reference location of the database.
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        
+        // Starttime
+        DateTime starttime = DateTime.Now;
+        
+        long randomID = UnityEngine.Random.Range(1000, 50000);
+        reference.Child("users").Child("id").SetValueAsync(randomID);
+        //
+        reference.Child("users").Child("correct").SetValueAsync(0);
+        reference.Child("users").Child("incorrect").SetValueAsync(0);
+        reference.Child("users").Child("result").SetValueAsync("0 of 0");
+        reference.Child("users").Child("timeelapsed").SetValueAsync(0);
     }
+    
+    void OnApplicationQuit()
+    {
+        DateTime currentTime = DateTime.Now;
+        TimeSpan difference = currentTime.Subtract(starttime);
+        long sessiontime = difference.Seconds;
+        //
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        reference.Child("users").Child("sessiontime").SetValueAsync(sessiontime);
+    }
+    
 }
